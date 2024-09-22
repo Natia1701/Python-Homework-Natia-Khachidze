087f7100593dff71400fe3ae8f5008ac26cc984b
Natia1701 <natia.khachidze17@gmail.com>
2024-09-22 19:05:41 +0400

    Update task_4.py

--- homework_3/task_4.py
+++ homework_3/task_4.py
@@ -1,7 +1,6 @@
-
 import random
 colors = ["club", "daimond",  "heart", "spader"]
 value = ["A", "K", "J", "Q", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
 random_value = random.choice(value)
 random_color = random.choice(colors)
-print (random_color, random_value)
+print(random_color, random_value)
