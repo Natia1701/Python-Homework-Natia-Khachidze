2bad78cd283d7ec850ae9da86dc08d3c786e7b5c
Natia1701 <natia.khachidze17@gmail.com>
2024-09-22 19:05:13 +0400

    Update task_3.py

--- homework_3/task_3.py
+++ homework_3/task_3.py
@@ -1,10 +1,9 @@
-year = int(input ("enter your year of birth- "))
-month = int(input ("enter your month of birth- "))
-day = int(input ("enter your day of birth- "))
-
 import datetime
+year = int(input("enter your year of birth- "))
+month = int(input("enter your month of birth- "))
+day = int(input("enter your day of birth- "))
 
-date = datetime.date (year, month, day)
+date = datetime.date(year, month, day)
 day_of_the_week = date.strftime("%A")
 
-print (day_of_the_week)
+print(day_of_the_week)
