cc04557b6e10e3cf862f0115a915dbaf4227cf51
Natia1701 <natia.khachidze17@gmail.com>
2024-09-24 09:33:23 +0400

    Update task_2.py

--- homework_4/task_2.py
+++ homework_4/task_2.py
@@ -8,10 +8,11 @@ if number > 0 and number < 30 :
         random_numbers = random.randint(0, 1000)
         if maximum_value <= random_numbers :
             maximum_value = random_numbers
+    print(maximum_value)
 else :
     print("please enter positive integer less then 30")
 
-print(maximum_value)
+
 
    
 
