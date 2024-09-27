e66fcbd14b550e9c955bc3a582ee5ecfa2c2181e
Natia1701 <natia.khachidze17@gmail.com>
2024-09-27 14:18:18 +0400

    practice_lecture_4

---
+++ practice_lecture_4/task_2.py
@@ -0,0 +1,7 @@
+#პროგრამამ მოგვთხოვოს სიტყვისა და რიცხვის შეყვანა და ეს სიტყვა დაბეჭდოს იმდენჯერ რა რიცხვსაც შევიყვანთ
+
+word = input("please enter your word - ")
+number = int(input("please enter number - "))
+
+for i in range(number):
+    print(word)
